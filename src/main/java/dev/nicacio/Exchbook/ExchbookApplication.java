e01fe6b2423f01eac0410e8a4906a255e8a969e0
gabrielNicacio-gn <dev.gabrielnicacio@gmail.com>
2025-03-16 13:16:07 -0300

    Initial Commit

---
+++ src/main/java/dev/nicacio/Exchbook/ExchbookApplication.java
@@ -0,0 +1,13 @@
+package dev.nicacio.Exchbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/nicacio/Exchbook/ExchbookApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nicacio.Exchbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
