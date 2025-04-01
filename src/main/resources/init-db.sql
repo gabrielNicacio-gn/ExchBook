8aafba82ee0646062e7bcba9f6239fdad2ae9183
gabrielNicacio-gn <dev.gabrielnicacio@gmail.com>
2025-04-01 10:20:01 -0300

    refactor: changing the type of PKs and FKs

--- src/main/resources/init-db.sql
+++ src/main/resources/init-db.sql
@@ -1,50 +1,50 @@
 
 
 CREATE TABLE user_owner(
-    id_user INT PRIMARY KEY,
+    id_user SERIAL PRIMARY KEY,
     email VARCHAR(80) UNIQUE NOT NULL,
     password VARCHAR(80) NOT NULL
 );
 
 CREATE TABLE user_roles(
-    id_roles INT PRIMARY KEY,
-    id_user INT NOT NULL,
+    id_roles SERIAL PRIMARY KEY,
+    id_user INTEGER NOT NULL,
     role VARCHAR(20) NOT NULL,
     FOREIGN KEY (id_user) REFERENCES user_owner(id_user)
 );
 
 CREATE TABLE book(
-    id_book INT PRIMARY KEY,
+    id_book SERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL
 );
 CREATE INDEX ON book(title);
 
 CREATE TABLE author(
-    id_author INT PRIMARY KEY,
+    id_author SERIAL PRIMARY KEY,
     name VARCHAR(100)
 );
 
 CREATE TABLE book_author(
-    id_book INT NOT NULL,
-    id_author INT NOT NULL,
+    id_book INTEGER NOT NULL,
+    id_author INTEGER NOT NULL,
     FOREIGN KEY (id_book) REFERENCES book(id_book),
     FOREIGN KEY (id_author) REFERENCES author(id_author)
 );
 
 CREATE TABLE book_copy(
-    id_book_copy INT PRIMARY KEY,
-    id_book INT NOT NULL,
-    id_user_owner INT NOT NULL,
+    id_book_copy SERIAL PRIMARY KEY,
+    id_book INTEGER NOT NULL,
+    id_user_owner INTEGER NOT NULL,
     condition VARCHAR(30),
     FOREIGN KEY (id_book) REFERENCES book(id_book),
     FOREIGN KEY (id_user_owner) REFERENCES user_owner(id_user)
 );
 
 CREATE TABLE book_review(
-     id_book_review INT PRIMARY KEY,
-     id_user_review INT NOT NULL,
-     id_book_evaluated INT NOT NULL,
-     score_book INT NOT NULL,
+     id_book_review SERIAL PRIMARY KEY,
+     id_user_review INTEGER NOT NULL,
+     id_book_evaluated INTEGER NOT NULL,
+     score_book INTEGER NOT NULL,
      comment TEXT NOT NULL,
      date_review TIMESTAMP NOT NULL,
      FOREIGN KEY (id_user_review) REFERENCES user_owner(id_user),
@@ -52,8 +52,8 @@ CREATE TABLE book_review(
 );
 
 CREATE TABLE book_edition(
-    id_book_edition INT PRIMARY KEY,
-    id_book INT NOT NULL,
+    id_book_edition SERIAL PRIMARY KEY,
+    id_book INTEGER NOT NULL,
     year_of_publication VARCHAR(10) NOT NULL,
     number_edition VARCHAR(30) NOT NULL,
     format VARCHAR(30) NOT NULL,
@@ -61,10 +61,10 @@ CREATE TABLE book_edition(
 );
 
 CREATE TABLE exchenge_offer(
-    id_exchenge_offer INT PRIMARY KEY,
-    id_copy_offered INT NOT NULL,
-    id_book_disired INT NOT NULL,
-    id_offering_user INT NOT NULL,
+    id_exchenge_offer SERIAL PRIMARY KEY,
+    id_copy_offered INTEGER NOT NULL,
+    id_book_disired INTEGER NOT NULL,
+    id_offering_user INTEGER NOT NULL,
     status_exchenge VARCHAR(30) NOT NULL,
     date_of_offer TIMESTAMP NOT NULL,
     FOREIGN KEY (id_copy_offered) REFERENCES book_copy(id_book_copy),
@@ -73,9 +73,9 @@ CREATE TABLE exchenge_offer(
 );
 
 CREATE TABLE exchenge(
-    id_exchenge INT PRIMARY KEY,
-    id_user_accepted INT NOT NULL,
-    id_exchenge_offer INT NOT NULL,
+    id_exchenge SERIAL PRIMARY KEY,
+    id_user_accepted INTEGER NOT NULL,
+    id_exchenge_offer INTEGER NOT NULL,
     is_approved BOOLEAN NOT NULL,
     date_of_offer TIMESTAMP NOT NULL,
     FOREIGN KEY (id_user_accepted) REFERENCES user_owner(id_user),
