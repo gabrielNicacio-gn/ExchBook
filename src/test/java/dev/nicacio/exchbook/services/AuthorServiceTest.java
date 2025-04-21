987f9e528318c94ef557b39939538259af4c6017
gabrielNicacio-gn <dev.gabrielnicacio@gmail.com>
2025-04-21 17:09:35 -0300

    test: test for GetAuthorById

--- src/test/java/dev/nicacio/exchbook/services/AuthorServiceTest.java
+++ src/test/java/dev/nicacio/exchbook/services/AuthorServiceTest.java
@@ -1,15 +1,20 @@
 package dev.nicacio.exchbook.services;
 
 import dev.nicacio.exchbook.dtos.request.CreateAuthorRequestDto;
+import dev.nicacio.exchbook.dtos.response.AuthorDto;
 import dev.nicacio.exchbook.mapper.AuthorMapper;
 import dev.nicacio.exchbook.models.Author;
 import dev.nicacio.exchbook.repository.AuthorRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -18,13 +23,13 @@ class AuthorServiceTest {
 
     @Mock
     private AuthorRepository authorRepository;
-    @Mock
-    private AuthorMapper authorMapper;
+    private final AuthorMapper authorMapper = Mappers.getMapper(AuthorMapper.class);
     @InjectMocks
     private AuthorService authorService;
     @BeforeEach
     void setup(){
         MockitoAnnotations.openMocks(this);
+        authorService = new AuthorService(authorRepository,authorMapper);
     }
     @Test
     public void shouldRegisterAnAuthor(){
@@ -34,7 +39,6 @@ class AuthorServiceTest {
         savedAuthor.setIdAuthor(1);
         savedAuthor.setName("NameAuthorOne");
 
-        when(authorMapper.toAuthor(create)).thenReturn(savedAuthor);
         when(authorRepository.save(any(Author.class))).thenReturn(savedAuthor);
 
         int idCreatedAuthor = authorService.registerAuthor(create);
@@ -42,5 +46,25 @@ class AuthorServiceTest {
         verify(authorRepository,times(1)).save(any());
         assertEquals("NameAuthorOne",savedAuthor.getName());
         assertEquals(savedAuthor.getIdAuthor(),idCreatedAuthor);
+    }
+
+    @Test
+    public void shouldGetAuthorById() throws ChangeSetPersister.NotFoundException {
+        int idAuthor = 2;
+
+        Optional<Author> author = Optional.of(new Author());
+        author.get().setIdAuthor(2);
+        author.get().setName("First Author");
+
+        AuthorDto authorDto = new AuthorDto(author.get().getIdAuthor(),author.get().getName());
+
+        when(authorRepository.findById(idAuthor)).thenReturn(author);
+
+        AuthorDto authorResponse = authorService.getAuthorById(2);
+
+        verify(authorRepository,times(1)).findById(any());
+        assertEquals(authorDto.idAuthor(),authorResponse.idAuthor());
+        assertEquals(authorDto.name(),authorResponse.name());
+
     }
 }
