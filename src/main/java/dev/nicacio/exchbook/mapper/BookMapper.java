cf6b540d5ff11a125c34d1db3c5956bde2419a9e
gabrielNicacio-gn <dev.gabrielnicacio@gmail.com>
2025-05-07 12:19:15 -0300

    refactor: Mapping UpdateDto from to the Entity

--- src/main/java/dev/nicacio/exchbook/mapper/BookMapper.java
+++ src/main/java/dev/nicacio/exchbook/mapper/BookMapper.java
@@ -1,13 +1,14 @@
 package dev.nicacio.exchbook.mapper;
 
 import dev.nicacio.exchbook.dtos.request.CreateBookRequestDto;
+import dev.nicacio.exchbook.dtos.request.UpdateBookRequestDto;
 import dev.nicacio.exchbook.dtos.response.BookDto;
 import dev.nicacio.exchbook.models.Author;
 import dev.nicacio.exchbook.models.Book;
+import dev.nicacio.exchbook.models.BookCopy;
 import dev.nicacio.exchbook.repository.AuthorRepository;
-import org.mapstruct.Context;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
+import org.springframework.context.annotation.Bean;
 
 import java.util.Collections;
 import java.util.List;
@@ -17,4 +18,6 @@ public interface BookMapper {
     //@Mapping(target = "authors", source = "authorIds")
     Book toBook (CreateBookRequestDto createBook, List<Author> authors);
     BookDto toBookDto(Book book);
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateBookFromDto(UpdateBookRequestDto updateDto, @MappingTarget Book book);
 }
