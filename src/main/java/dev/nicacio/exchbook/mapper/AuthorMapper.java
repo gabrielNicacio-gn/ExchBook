c27d654613e5b0c50d8478387741e0cf855d069e
gabrielNicacio-gn <dev.gabrielnicacio@gmail.com>
2025-05-07 12:20:27 -0300

    Improving the mapping from Dto to Entity

--- src/main/java/dev/nicacio/exchbook/mapper/AuthorMapper.java
+++ src/main/java/dev/nicacio/exchbook/mapper/AuthorMapper.java
@@ -4,8 +4,10 @@ import dev.nicacio.exchbook.dtos.request.CreateAuthorRequestDto;
 import dev.nicacio.exchbook.dtos.request.UpdateAuthorRequestDto;
 import dev.nicacio.exchbook.dtos.response.AuthorDto;
 import dev.nicacio.exchbook.models.Author;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 import org.springframework.context.annotation.Bean;
 
@@ -13,5 +15,6 @@ import org.springframework.context.annotation.Bean;
 public interface AuthorMapper {
     Author toAuthor(CreateAuthorRequestDto requestDto);
     AuthorDto toAuthorDto(Author author);
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateAuthorFromDto(UpdateAuthorRequestDto update, @MappingTarget Author author);
 }
